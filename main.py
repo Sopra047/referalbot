00d9902ed1a2b7c2e09f127763ebf0cde9b1d9f7
Sopra047 <111066382+Sopra047@users.noreply.github.com>
2022-08-15 16:08:11 +0200

    Update main.py

--- main.py
+++ main.py
@@ -154,12 +154,11 @@ def query_handler(call):
             markup = telebot.types.InlineKeyboardMarkup()
             markup.add(telebot.types.InlineKeyboardButton(
                 text='✅ Verifier', callback_data='check'))
-            msg_start = "*🍔 Pour utiliser ce bot, vous devez rejoindre cette chaîne en cliquant sur ce lien 👉 - \n➡️ @MTNreferral \n➡️ @SharClub8 *"
+            msg_start = "*🍔 Pour utiliser ce bot, vous devez rejoindre cette chaîne en cliquant sur ce lien 👉 - \n➡️ @MTNreferral \n➡️ @SharClub8*"
             bot.send_message(call.message.chat.id, msg_start,
                              parse_mode="Markdown", reply_markup=markup)
    except:
-        bot.send_message(call.message.chat.id, "Cette commande ayant une erreur, veuillez attendre que le problème soit corrigé par l'administrateur")
-        bot.send_message(OWNER_ID, "Votre bot a rencontré une erreur, corrigez-la rapidement !\n Erreur lors de la commande: "+call.data)
+
         return
 
 @bot.message_handler(content_types=['text'])
