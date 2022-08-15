ae15c9b0e7a71c2c904b1a864fd9be0ca4e5f1f0
Sopra047 <111066382+Sopra047@users.noreply.github.com>
2022-08-15 16:05:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ TOKEN = "FCFA"
 BOT_TOKEN = "5555491322:AAGlbXGRTo-nGDKAXBlyPy-ixzKepLboDUM"
 PAYMENT_CHANNEL = "@MTNreferral" #add payment channel here including the '@' sign
 OWNER_ID = 5270469995 #write owner's user id here.. get it from @MissRose_Bot by /id
-CHANNELS = ["@MTNreferral" , "@SharClu8"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
+CHANNELS = ["@MTNreferral" , "@SharClub8"] #add channels to be checked here in the format - ["Channel 1", "Channel 2"] 
               #you can add as many channels here and also add the '@' sign before channel username
 Daily_bonus = 250 #Put daily bonus amount here!
 Mini_Withdraw = 20000  #remove 0 and add the minimum withdraw u want to set
